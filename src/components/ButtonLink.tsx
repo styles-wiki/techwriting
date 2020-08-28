127079c15851577ee174b669715ecd74db993e55
Mark Larah <mark@larah.me>
2020-08-27 23:22:10 -0700

    fix typescript

--- src/components/ButtonLink.tsx
+++ src/components/ButtonLink.tsx
@@ -1,6 +1,14 @@
-import { forwardRef } from 'react';
+import { ReactNode, forwardRef } from 'react';
 
-const ButtonLink = forwardRef(({ href, onClick, children }, ref) => {
+interface Props {
+    href?: string;
+    onClick?: () => void;
+    children: ReactNode;
+}
+
+const ButtonLink = forwardRef<HTMLAnchorElement, Props>((props, ref) => {
+    const { href, onClick, children } = props;
+
     return (
         <a href={href} onClick={onClick} ref={ref}>
             <div className="rounded px-3 pt-2 pb-1 bg-gray-100 hover:bg-purple-100">{children}</div>
